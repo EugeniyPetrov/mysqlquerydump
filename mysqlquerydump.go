7da89715e89b1d50acf4c487eaadf8412ad125d2
Eugeniy Petrov <eug.a.petrov@gmail.com>
2016-05-08 20:35:15 +0100

    Long cli options. Restore charset after sql dump

--- mysqlquerydump.go
+++ mysqlquerydump.go
@@ -6,10 +6,10 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"errors"
-	"flag"
 	"fmt"
 	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/mkideal/cli"
 	"gopkg.in/ini.v1"
 	"io"
 	"log"
@@ -27,6 +27,7 @@ type MysqlOptions struct {
 	Database string
 	Port     uint16
 	Charset  string
+	Timezone string
 }
 
 func (options *MysqlOptions) Extend(extra *MysqlOptions) {
@@ -47,6 +48,9 @@ func (options *MysqlOptions) Extend(extra *MysqlOptions) {
 	}
 	if extra.Charset != "" {
 		options.Charset = extra.Charset
+	}
+	if extra.Timezone != "" {
+		options.Timezone = extra.Timezone
 	}
 }
 
@@ -317,7 +321,13 @@ func outSql(
 		}
 	}
 
-	out.Write([]byte("SET NAMES " + options.Charset + ";\n\n"))
+	out.Write([]byte("SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT;\n"))
+	out.Write([]byte("SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS;\n"))
+	out.Write([]byte("SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION;\n"))
+	out.Write([]byte("SET NAMES " + options.Charset + ";\n"))
+	//out.Write([]byte("SET @OLD_TIME_ZONE=@@TIME_ZONE;\n"))
+	//out.Write([]byte("SET TIME_ZONE='+00:00';\n"))
+	out.Write([]byte("\n"))
 
 	var sqlBuffer bytes.Buffer
 
@@ -376,54 +386,114 @@ func outSql(
 		sqlBuffer.WriteTo(out)
 	}
 
+	out.Write([]byte("\n"))
+	//out.Write([]byte("SET TIME_ZONE=@OLD_TIME_ZONE;\n"))
+	out.Write([]byte("SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT;\n"))
+	out.Write([]byte("SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS;\n"))
+	out.Write([]byte("SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION;\n"))
+
 	return nil
 }
 
-func main() {
-	host := flag.String("h", "", "Connect to host.")
-	user := flag.String("u", "", "User for login if not current user.")
-	database := flag.String("D", "", "Database to use.")
-	port64 := flag.Uint64("P", 0, "The TCP/IP port number to use for the connection.")
-	query := flag.String("e", "", "The query to be processed. If not specified it will be given from standart input. It is recommended to use the command with outer sql-file.")
-	format := flag.String("f", "csv", "Query output format. Possible values: csv, sql, json.")
-	alias := flag.String("a", "", "MySQL table alias the result of a query will by written in. It is so pointless with the -f csv.")
-	insertIgnore := flag.Bool("i", false, "Produce INSERT IGNORE output for sql dump.")
-	onDuplicateKeyUpdate := flag.Bool("U", false, "Produce statement for update duplicate rows.")
-	batchSize := flag.Int("s", 1024, "Batch size in kb")
-	configFile := flag.String("c", "", "configuration ini file")
+type mysqlquerydumpT struct {
+	Help                 bool   `cli:"!help" usage:"display help information"`
+	Host                 string `cli:"h,host" usage:"Connect to host."`
+	User                 string `cli:"u,user" usage:"User for login."`
+	Database             string `cli:"D,database" usage:"Database to use."`
+	Port                 uint16 `cli:"P,port" usage:"The TCP/IP port number to use for the connection."`
+	Query                string `cli:"q,query" usage:"The query to be processed. If not specified it will be given from standart input. It is recommended to use the command with outer sql-file."`
+	Format               string `cli:"f,format" usage:"Query output format. Possible values: csv, sql, json."`
+	Alias                string `cli:"a,alias" usage:"MySQL table alias the result of a query will by written in. It is so pointless with the -f csv."`
+	InsertIgnore         bool   `cli:"i,insert-ignore" usage:"Produce INSERT IGNORE output for sql dump."`
+	OnDuplicateKeyUpdate bool   `cli:"U,on-duplicate-key-update" usage:"Produce statement for update duplicate rows."`
+	BatchSize            int    `cli:"s,batch-size" usage:"Batch size in kb"`
+	ConfigFile           string `cli:"c,config-file"`
+}
 
-	flag.Parse()
+var app = &cli.Command{
+	Name: os.Args[0],
+	Desc: "mysqlquerydump - a program to dump query result in different formats",
+	Argv: func() interface{} {
+		return new(mysqlquerydumpT)
+	},
+	Fn: mysqlquerydump,
+}
+
+func mysqlquerydump(ctx *cli.Context) error {
+	argv := ctx.Argv().(*mysqlquerydumpT)
+
+	if argv.Help {
+		ctx.WriteUsage()
+		return nil
+	}
+
+	if argv.Query == "" {
+		return errors.New("--query parameter is required.")
+	}
+
+	if argv.Format == "" {
+		return errors.New("--format parameter is required.")
+	}
 
 	options, err := getDbOptions(
-		*host,
-		*user,
-		*database,
-		uint16(*port64),
-		*configFile,
+		argv.Host,
+		argv.User,
+		argv.Database,
+		argv.Port,
+		argv.ConfigFile,
 	)
-	failOnError(err)
+
+	if err != nil {
+		return err
+	}
 
 	db, err := getDb(options)
-	failOnError(err)
+	if err != nil {
+		return err
+	}
 	defer db.Close()
 
-	stmt, err := db.Prepare(*query)
-	failOnError(err)
+	stmt, err := db.Prepare(argv.Query)
+	if err != nil {
+		return err
+	}
 	defer stmt.Close()
 
 	rows, err := stmt.Query()
-	failOnError(err)
+	if err != nil {
+		return err
+	}
 
 	out := os.Stdout
 
-	switch *format {
+	switch argv.Format {
 	case "json":
 		err = outJson(out, rows)
 	case "csv":
 		err = outCsv(out, rows)
 	case "sql":
-		err = outSql(out, rows, *alias, *insertIgnore, *onDuplicateKeyUpdate, *batchSize, options)
+		err = outSql(out, rows, argv.Alias, argv.InsertIgnore, argv.OnDuplicateKeyUpdate, argv.BatchSize, options)
+	default:
+		return errors.New(fmt.Sprintf("Unknown format \"%s\"", argv.Format))
 	}
 
-	failOnError(err)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// @todo read query from stdin
+// @todo write man
+// @todo set timezone
+// @todo create table statement
+func main() {
+	cli.SetUsageStyle(cli.ManualStyle)
+
+	if err := app.RunWith(os.Args[1:], os.Stderr, nil); err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	os.Exit(1)
 }
